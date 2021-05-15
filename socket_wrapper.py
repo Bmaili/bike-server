1c85695aac0c734cca0191c123bb9ce1e953c778
maili <2228921046@qq.com>
2021-05-15 18:05:06 +0800

    发送数据后加上\r\n以解决跟java客户端的socket的传输问题

--- socket_wrapper.py
+++ socket_wrapper.py
@@ -2,7 +2,6 @@ class SocketWrapper(object):
     """包装客户端套接字类"""
 
     def __init__(self, sock):
-
         self.sock = sock
 
     def recv_data(self):
@@ -13,6 +12,7 @@ class SocketWrapper(object):
     def send_data(self, messages):
         """发送数据"""
 
+        messages += "\r\n"
         return self.sock.send(messages.encode("utf-8"))
 
     def close(self):
